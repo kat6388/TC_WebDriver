b1f0e3d546f5d6efe6f5ea5506a2d5b0e5bfd272
Katya Astakhova <KatyaAstakhova@coherentsolutions.com>
2019-02-13 15:20:15 +0300

    Task 50

---
+++ Task20/ClassLibrary1/Tests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        public const string url = "https://www.tut.by/";
+        public const string username = "seleniumtests@tut.by";
+        public const string password = "123456789zxcvbn";
+
+        public const string frameUrl = "https://the-internet.herokuapp.com/iframe";
+        public const string alertUrl = "https://the-internet.herokuapp.com/javascript_alerts";
+
+        [Test]
+        public void Login()
+        {
+            var driver = Drivers.Methods.NavigateTo(url);
+            var actualUrl = Drivers.Methods.GetUrl(driver);
+            Assert.IsTrue(actualUrl.StartsWith(url), "invalid url)");
+
+            Drivers.Methods.PopulateLoginForm(driver, username, password);
+
+            var submitButtonState = Drivers.Methods.IsLoginButtonEnabled(driver);
+            Assert.IsTrue(submitButtonState);
+
+            Drivers.Methods.LoginUser(driver);
+
+            var loginState = Drivers.Methods.IsUserLogin(driver);
+            
+            Assert.IsTrue(loginState);
+        }
+
+        [Test]
+        public void TimeOut()
+        {
+            var driver = Drivers.Methods.NavigateTo(url);
+            Drivers.Methods.PopulateLoginForm(driver, username, password);
+            Drivers.Methods.LoginUser(driver);
+            Drivers.Methods.WaitUntilElementDisplayed(driver, By.XPath(Drivers.Methods.authorizedUserLocator));
+        }
+
+        [Test]
+        public void FromExcel()
+        {
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook workbook = app.Workbooks.Open(@"C:\Users\katyaastakhova\source\repos\TC_WebDriver\Task20\DDT4.xlsx");
+            Excel.Worksheet worksheet = (Excel.Worksheet)app.Worksheets["DataSet"];
+            Excel.Range range = worksheet.UsedRange;
+
+            for (int i = 1; i <= 2; i++)
+            {
+                var driver = Drivers.Methods.NavigateTo(url);
+                string username;
+                string password;
+                username = range.Cells[1][i].value2;
+                password = range.Cells[2][i].value2;
+
+                Drivers.Methods.LoginSeveralUsers(driver, username, password);
+                var loginState = Drivers.Methods.IsUserLogin(driver);
+                Assert.IsTrue(loginState);
+                driver.Close();
+            }
+        }
+
+        [Test]
+            public void IframeTests()
+        {
+            var driver = Drivers.Methods.NavigateTo(frameUrl);
+            var actual = Drivers.Methods.PrintText(driver);
+            var expected = "Hello \uFEFFworld!";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void JsAlertTest()
+        {
+            var driver = Drivers.Methods.NavigateTo(alertUrl);
+            var actualAcceptResultMessage = Drivers.Methods.ClickOnJsAlert(driver);
+            var expectedAcceptResultMessage = "You successfuly clicked an alert";
+            Assert.AreEqual(expectedAcceptResultMessage, actualAcceptResultMessage);
+        }
+
+        [Test]
+        public void JsConfirmAcceptTest()
+        {
+            var driver = Drivers.Methods.NavigateTo(alertUrl);
+            var actualConfirmResultMessage = Drivers.Methods.ClickOKJsConfirm(driver);
+            var expectedConfirmResultMessage = "You clicked: Ok";
+            Assert.AreEqual(expectedConfirmResultMessage, actualConfirmResultMessage);
+            var actualCancelResultMessage = Drivers.Methods.ClickCancelOnJsConfirm(driver);
+            var expectedCancelResultMessage = "You clicked: Cancel";
+            Assert.AreEqual(expectedCancelResultMessage, actualCancelResultMessage);
+        }
+        [Test]
+        public void JsPromptTest()
+        {
+            var driver = Drivers.Methods.NavigateTo(alertUrl);
+            var actualConfirmResultMessage = Drivers.Methods.ClickJsPromptConfirm(driver);
+            var expectedConfirmResultMessage = "You entered:";
+            Assert.AreEqual(expectedConfirmResultMessage, actualConfirmResultMessage);
+            var actualCancelResultMessage = Drivers.Methods.ClickJsPromptCancel(driver);
+            var expectedCancelResultMessage = "You entered: null";
+            Assert.AreEqual(expectedCancelResultMessage, actualCancelResultMessage);
+            var actualResultText = Drivers.Methods.ClickJsPromptText(driver);
+            var expectedResultText = driver.FindElement(By.XPath("//p[@id='result']")).Text;
+            Assert.AreEqual(expectedResultText, actualResultText);
+        }
+    }
+}
---
+++ Task20/ClassLibrary2/Methods.cs
@@ -0,0 +1,146 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace Drivers
+{
+    public class Methods
+    {
+        public const string loginLinkLocator = "//a[.='Войти']";
+        public const string usernameTextfieldLocator = "//input[@name = 'login']";
+        public const string passwordTextfieldLocator = "//input[@name = 'password']";
+        public const string loginButtonLocator = "//input[@value='Войти']";
+        public const string authorizedUserLocator = "//span[@class='uname']";
+        public const string frame = "Hello world!";
+        public const string property = "//a[.='Войти']";
+        public static IWebDriver NavigateTo(string url)
+        {
+            var chromeDriver = new ChromeDriver(@"D:\Automation");
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            chromeDriver.Navigate().GoToUrl(url);
+            return chromeDriver;
+        }
+
+        public static string GetUrl(IWebDriver chromeDriver)
+        {
+            return chromeDriver.Url;
+        }
+
+        public static void PopulateLoginForm(IWebDriver chromeDriver, string username, string password)
+        {
+            Actions builder = new Actions(chromeDriver);
+
+            chromeDriver.FindElement(By.XPath(loginLinkLocator)).Click();
+            chromeDriver.FindElement(By.XPath(usernameTextfieldLocator)).SendKeys(username);
+            builder.SendKeys(Keys.Tab).Perform();
+            Thread.Sleep(2000);
+            chromeDriver.FindElement(By.XPath(passwordTextfieldLocator)).SendKeys(password);
+        }
+
+        public static bool IsLoginButtonEnabled(IWebDriver chromeDriver)
+        {
+            return chromeDriver.FindElement(By.XPath(loginButtonLocator)).Enabled;
+        }
+
+        public static void LoginUser(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.XPath(loginButtonLocator)).Click();
+        }
+
+        public static bool IsUserLogin(IWebDriver chromeDriver)
+        {
+            return chromeDriver.FindElement(By.XPath(authorizedUserLocator)).Displayed;
+        }
+
+        public static void WaitUntilElementDisplayed(IWebDriver chromeDriver, By by)
+        {
+            var wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(1));
+            var element = wait.Until(condition =>
+            {
+                try
+                {
+                    var elementToBeDisplayed = chromeDriver.FindElement(by);
+                    return elementToBeDisplayed.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public static void LoginSeveralUsers(IWebDriver chromeDriver, string username, string password)
+        {
+            chromeDriver.FindElement(By.XPath(loginLinkLocator)).Click();
+            chromeDriver.FindElement(By.XPath(usernameTextfieldLocator)).SendKeys(username.ToString());
+            chromeDriver.FindElement(By.XPath(passwordTextfieldLocator)).SendKeys(password + Keys.Enter);
+        }
+
+        public static string PrintText (IWebDriver chromeDriver)
+        {
+            var textField = chromeDriver.FindElement(By.XPath("//iframe[@id = 'mce_0_ifr']"));
+            textField.Click();
+            textField.SendKeys(Keys.Control + "a" + Keys.Backspace);
+            textField.SendKeys("Hello ");
+            chromeDriver.FindElement(By.XPath("//i[@class = 'mce-ico mce-i-bold']")).Click();
+            textField.SendKeys("world!");
+            chromeDriver.SwitchTo().Frame(textField);
+            var entertedText = chromeDriver.FindElement(By.XPath("html/body/p")).Text;
+            return entertedText;
+        }
+       
+        public static string ClickOnJsAlert(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.XPath("//button[@onclick = 'jsAlert()']")).Click();
+            chromeDriver.SwitchTo().Alert().Accept();
+            var result = chromeDriver.FindElement(By.XPath("//p[@id='result']")).Text;
+            return result;
+        }
+
+        public static string ClickCancelOnJsConfirm(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.XPath("//button[@onclick = 'jsConfirm()']")).Click();
+            chromeDriver.SwitchTo().Alert().Dismiss();
+            var result = chromeDriver.FindElement(By.XPath("//p[@id='result']")).Text;
+            return result;
+        }
+        public static string ClickOKJsConfirm(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.XPath("//button[@onclick = 'jsConfirm()']")).Click();
+            chromeDriver.SwitchTo().Alert().Accept();
+            var result = chromeDriver.FindElement(By.XPath("//p[@id='result']")).Text;
+            return result;
+        }
+
+        public static string ClickJsPromptConfirm(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.XPath("//button[@onclick = 'jsPrompt()']")).Click();
+            chromeDriver.SwitchTo().Alert().Accept();
+            var result = chromeDriver.FindElement(By.XPath("//p[@id='result']")).Text;
+            return result;
+        }
+        public static string ClickJsPromptCancel(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.XPath("//button[@onclick = 'jsPrompt()']")).Click();
+            chromeDriver.SwitchTo().Alert().Dismiss();
+            var result = chromeDriver.FindElement(By.XPath("//p[@id='result']")).Text;
+            return result;
+        }
+        public static string ClickJsPromptText(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.XPath("//button[@onclick = 'jsPrompt()']")).Click();
+            var alertText = "Text";
+            chromeDriver.SwitchTo().Alert().SendKeys(alertText);
+            chromeDriver.SwitchTo().Alert().Accept();
+            return "You entered: " + alertText;
+        }
+    }
+}
+
